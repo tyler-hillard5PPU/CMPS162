726b173748b074f0e5a33fe5b440b565b0bee011
tyler-hillard5PPU <72617633+tyler-hillard5PPU@users.noreply.github.com>
2021-04-23 21:06:04 -0400

    Add files via upload

---
+++ hideimg.js
@@ -0,0 +1,7 @@
+function hide(){
+    document.getElementById('logo').className = "hidden";
+    document.getElementById('ben').className = "hidden";
+    document.getElementById('juju').className = "hidden";
+    document.getElementById('minkah').className = "hidden";
+    document.getElementById('tj').className = "hidden";
+  }
